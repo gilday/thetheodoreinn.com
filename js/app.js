56894e29ff1cb047ff7722742252ab8e04d95329
Johnathan Gilday <me@johnathangilday.com>
2014-11-23 22:50:51 -0500

    Added Google AppScript and Google Plus Signin

--- js/app.js
+++ js/app.js
@@ -5,26 +5,57 @@
         // Update the app to reflect a signed in user
         // Hide the sign-in button now that the user is authorized
         document.getElementById('signinButton').setAttribute('style', 'display: none');
-        getProfile(onProfileReceived);
+        getProfile().then(onProfileReceived);
       }
     }
 
-    function getProfile(callback) {
+    /**
+     * submits the booking form and returns a promise
+     */
+    function bookRoom() {
+      var booking = $('form').serialize();
+      console.log(booking);
+      var appsScriptUrl = 'https://script.google.com/macros/s/AKfycbzGasb7tXx76ziNTalytCPUSAWgrxvYIn4LSHecMrEAcuqDGahu/exec';
+      return $.ajax(appsScriptUrl, {
+        type: 'POST',
+        data: booking,
+        dataType: 'jsonp',
+        crossDomain: true
+      });
+    }
+
+    function getProfile() {
+      var deferred = $.Deferred();
       gapi.client.load('plus','v1', function(){
         var request = gapi.client.plus.people.get({
            'userId': 'me'
         });
-        request.execute(callback);
+        request.execute(deferred.resolve);
       });
+      return deferred.promise();
     }
 
     function onProfileReceived(profile) {
       var setValue = function(id, value) {
-        $('#' + id).hide().text(value).fadeIn();
+        $('#' + id).val(value);
       };
       setValue('name', profile.displayName);
       setValue('email', profile.emails[0].value);
     }
 
+    function configure() {
+      $('#booking-form').submit(function() {
+        bookRoom().then(showSuccess);
+        return false;
+      });
+    } 
+
+    function showSuccess() {
+      console.log('success');
+      $('form').fadeOut();
+      $('#thank-you-message').show();
+    }   
+
+    configure();
     window.onSignin = onSignin; // make available to global scope
 })(this.jQuery, this);
